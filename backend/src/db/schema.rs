5252797a28013e80aae5da2e5987bfc8e402dc4f
Sandu Crucerescu <alexcrucerescu82@gmail.com>
2023-02-06 17:18:40 +0100

    Added some migration

--- backend/src/db/schema.rs
+++ backend/src/db/schema.rs
@@ -1,27 +1,10 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
-    users(user_id) {
-        user_id -> Varchar,
-        name -> Varchar,
-        email -> Varchar,
-        password -> Varchar,
-        created_at -> Timestamptz,
-        updated_at -> Timestamptz,
+    posts (id) {
+        id -> Int4,
+        title -> Varchar,
+        body -> Text,
+        published -> Bool,
     }
 }
-
-diesel::table! {
-    transactions(id) {
-        id -> Varchar,
-        sender_id -> Varchar,
-        receiver_id -> Varchar,
-        amount -> Float,
-        // created_at -> Nullable<Timestamptz>,
-        // received_at -> Nullable<Timestamptz>,
-    }
-}
-
-// diesel::joinable!(transactions -> users (sender_id));
-
-diesel::allow_tables_to_appear_in_same_query!(users, transactions,);
